7ae668dea791b447a0aa405337d1ccdba250e3b7
chinmayeenb25 <257099448+chinmayeenb25@users.noreply.github.com>
2026-06-27 11:59:00 +0530

    created a function for varied grid grid size

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 console.log("Script loaded");
-const container=document.querySelector('.container');
-for (let i=0;i<16;i++){
-    const row=document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    
-    for(let i=0;i<16;i++){
-        const box=document.createElement("div");
-        box.classList.add("box");
-        row.append(box);
-        box.addEventListener("mouseover",()=>{
-            box.style.backgroundColor = "black";
+const size=document.querySelector('.grid-size');
+size.addEventListener("click",()=>{
+    let boxsize=prompt("enter the grid size");
+    boxside=Number(boxsize);
+    if (boxside<100){
+        createGrid(boxside);
+    }
+    else{
+        alert("The gride size should be less than 100");
+    }
+})
+function createGrid(x){
+    const container=document.querySelector('.container');
+    for (let i=0;i<x;i++){
+        const row=document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        
+        for(let i=0;i<x;i++){
+            const box=document.createElement("div");
+            box.classList.add("box");
+            box.style.width = `${960 / x}px`;
+            box.style.height = `${960 / x}px`;
+            row.append(box);
+            box.addEventListener("mouseover",()=>{
+                box.style.backgroundColor = "black";
 
-        });
+            });
+        }
     }
 }
+
 
